479fad25ce7e9f4371ef77290b708cc7538de75b
Pratims10 <40699847+Pratims10@users.noreply.github.com>
2019-11-12 00:22:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function showcomparisonbar(u1,u2,rt1,rt2,mrt1,mrt2)
+{
+	var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text:`Comparison of ${u1} vs ${u2}`
+	},
+	axisY: {
+		title: `Rating`,
+		titleFontColor: "#4F81BC",
+		lineColor: "#4F81BC",
+		labelFontColor: "#4F81BC",
+		tickColor: "#4F81BC"
+	},
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor:"pointer",
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "column",
+		name: `${u1}`,
+		legendText: `${u1}`,
+		showInLegend: true, 
+		dataPoints:[
+			{ label: "Current Rating", y: rt1 },
+			{ label: "Maximum Rating", y: mrt1 }
+		]
+	},
+	{
+		type: "column",
+		name: `${u2}`,
+		legendText: `${u2}`,
+	//	axisYType: "secondary",
+		showInLegend: true,
+		dataPoints:[
+			{ label: "Rating", y: rt2 },
+			{ label: "Max Rating", y: mrt2 }
+		]
+	}]
+});
+chart.render();
+function toggleDataSeries(e) {
+	if (typeof(e.dataSeries.visible) == "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+}
+   	function myfunc(){
+ //   	console.log("asa");
+		var u1=document.getElementById('user1').value;
+		var u2=document.getElementById('user2').value;
+		var rt1,rt2,mrt1,mrt2;
+	    $.getJSON(`https://codeforces.com/api/user.info?handles=${u1};${u2}`, function(data) {
+	    	rt1=data.result[0]["rating"];
+	    	rt2=data.result[1]["rating"];
+			mrt1=data.result[0]["maxRating"];
+	    	mrt2=data.result[1]["maxRating"];
+	        if(data.status!='OK')
+	        {
+	        	alert("Sorry, some error has occured.");
+	        }
+	        else
+	        {
+		        var str="";
+		        str=str.concat('<table style="width:50%; font-family:Sans-serif;margin-left:0%">');
+		        str=str.concat(`<tr style='background-color:#f2f2f2'><td>&nbsp;&nbsp;&nbsp;Username</td><td>`);
+		        str=str.concat(`<b>${u1}</b></td><td><b>${u2}</b></td></tr>`);
+		        str=str.concat(`<tr'><td>&nbsp;&nbsp;&nbsp;Current Rating</td><td>`);
+		        //rating
+		        str=str.concat(`${rt1}</td><td>${rt2}</td></tr>`);
+		        str=str.concat(`<tr style='background-color:#f2f2f2'><td>&nbsp;&nbsp;&nbsp;Max Rating</td><td>`);
+		        str=str.concat(`${mrt1}</td><td>${mrt2}</td></tr>`);
+		        str=str.concat(`<tr><td>&nbsp;&nbsp;&nbsp;Current Rank</td><td>`);
+		        str=str.concat(`<b>${data.result[0]["rank"]}</b></td><td><b>${data.result[1]["rank"]}</b></td></tr>`);
+		        str=str.concat(`<tr style='background-color:#f2f2f2'><td>&nbsp;&nbsp;&nbsp;Max Rank</td><td>`);
+		        str=str.concat(`<b>${data.result[0]["maxRank"]}</b></td><td><b>${data.result[1]["maxRank"]}</b></td></tr>`);
+		        str=str.concat('</table>');
+			    showcomparisonbar(u1,u2,rt1,rt2,mrt1,mrt2);
+		        $(".mypanel").html(str);
+		        document.getElementById('chartContainer').scrollIntoView();
+	    	}
+	    });
+//	    console.log("A");
+	}
+function redirect()
+{
+	location.replace('file:///C:/Users/PRATIM/Desktop/WEB%20DEV%20COURSERA/React%20Udemy/compare.html');
+}
---
+++ script2.js
@@ -0,0 +1,95 @@
+var lst=[{
+	name:"diuwuesgdf",
+	rating:-832761
+	}
+];
+function compare(a,b)
+{
+	if(a.rating>b.rating)
+		return -1;
+	else return 1;
+}
+function compare2(a,b)
+{
+	if(a.name>b.name)
+		return -1;
+	else return 1;
+}
+function add()
+{
+	document.getElementById("btnadd").disabled = true;
+	let s=document.getElementById('username').value;
+	s=s.trim();
+	s=s.toLowerCase();
+	let flag=true;
+	for(i=0;i<lst.length;i++)
+	{
+		if(lst[i]["name"].toLowerCase()==s)
+			flag=false;
+	}
+	if(flag==true)
+	{
+		add1();
+		add2();
+	}
+	else
+	{
+		add2();
+	}
+	document.getElementById("btnadd").disabled = false;
+}
+function add1()
+{
+	var u1=document.getElementById('username').value;
+	u1=u1.trim();
+	let x;
+	$.getJSON(`https://codeforces.com/api/user.info?handles=${u1}`, function(data) {
+		if(data.status[0]=="FAILED")
+		{
+			alert("Some error occured");
+			return ;
+		}
+		x={	name : data.result[0]["handle"], rating : data.result[0]["rating"]};
+		lst.push(x);
+	});
+}
+function check(list)
+{
+	list.sort(compare2);
+	let a=[];
+	let ctr=0;
+	for(i=0;i<list.length;i++)
+	{
+		a.push(list[i]);
+		while(i<list.length && list[i]["name"]===a[ctr]["name"])
+		{
+			i++;
+		}
+		alert("d"+a[ctr].name);
+		ctr++;
+	}
+	a.sort(compare);
+	lst=a;
+	alert(lst.length);
+}
+function add2()
+{
+	lst.sort(compare);
+	let list=lst;
+	alert(list[0]["name"]);
+	check(list);
+	var str="<table>";
+	for(i=0;i<lst.length-1;i++)
+	{
+		if(i%2==0)
+		str=str.concat(`<tr><td>${i+1}</td><td>${lst[i]["name"]}</td><td>${lst[i]["rating"]}</td></tr>`);
+	else
+		str=str.concat(`<tr style='background-color:#f2f2f2'><td>${i+1}</td><td>${lst[i]["name"]}</td><td>${lst[i]["rating"]}</td></tr>`);
+	}
+	str=str.concat(`</table>`);
+	$('.output').html(str);
+}
+function redirect()
+{
+	location.replace('file:///C:/Users/PRATIM/Desktop/WEB%20DEV%20COURSERA/React%20Udemy/index.html');
+}
